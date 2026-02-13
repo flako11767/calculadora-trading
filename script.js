09a70970a5725217addd6d5c65c60d6608349c0a
flako11767 <gabrielbergita78@gmail.com>
2026-02-13 13:46:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 function calcular() {
 
-let capital = parseFloat(document.getElementById("capital").value);
-let riesgo = parseFloat(document.getElementById("riesgo").value);
-let recompensa = parseFloat(document.getElementById("recompensa").value);
+let capital = Number(document.getElementById("capital").value);
+let riesgo = Number(document.getElementById("riesgo").value);
+let recompensa = Number(document.getElementById("recompensa").value);
+
+if(!capital || !riesgo || !recompensa){
+alert("Llena todos los campos");
+return;
+}
 
 let dineroRiesgo = capital * (riesgo / 100);
 let ganancia = capital * (recompensa / 100);
 
 document.getElementById("resultado").innerHTML =
-"Riesgo: $" + dineroRiesgo.toFixed(2) + "<br>Ganancia: $" + ganancia.toFixed(2);
+"💸 Riesgo: $" + dineroRiesgo.toFixed(2) +
+"<br>💰 Ganancia esperada: $" + ganancia.toFixed(2);
 
 }
