80ef2d4b0fcb259bc9fbf4c117797907ef8af6e6
flako11767 <gabrielbergita78@gmail.com>
2026-02-13 14:03:46 -0600

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ function calcular() {
 let capital = Number(document.getElementById("capital").value);
 let riesgo = Number(document.getElementById("riesgo").value);
 let recompensa = Number(document.getElementById("recompensa").value);
+let stop = Number(document.getElementById("stop").value);
+let pip = Number(document.getElementById("pip").value);
 
-if(!capital || !riesgo || !recompensa){
+if(!capital || !riesgo || !recompensa || !stop || !pip){
 alert("Llena todos los campos");
 return;
 }
@@ -12,8 +14,11 @@ return;
 let dineroRiesgo = capital * (riesgo / 100);
 let ganancia = capital * (recompensa / 100);
 
+let lotes = dineroRiesgo / (stop * pip);
+
 document.getElementById("resultado").innerHTML =
 "💸 Riesgo: $" + dineroRiesgo.toFixed(2) +
-"<br>💰 Ganancia esperada: $" + ganancia.toFixed(2);
+"<br>💰 Ganancia esperada: $" + ganancia.toFixed(2) +
+"<br>📐 Lotes recomendados: " + lotes.toFixed(2);
 
 }
